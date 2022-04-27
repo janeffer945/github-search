d4a2cfd03a51a78b3b263ece614898db2435959c
janeffer945 <janeffernjeri945@gmail.com>
2022-04-28 01:05:32 +0300

    modified module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { UserResultComponent } from './user-result/user-result.component';
 import { AppHighlightDirective } from './app-highlight.directive';
 import { DatePipePipe } from './date-pipe.pipe';
-
 @NgModule({
   declarations: [
     AppComponent,
