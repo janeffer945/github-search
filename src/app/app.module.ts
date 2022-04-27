5fb19710d4af8822a5af0cbe89c3466f78bfdb9c
janeffer945 <janeffernjeri945@gmail.com>
2022-04-28 00:32:08 +0300

    add modified user-result

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchFormComponent } from './search-form/search-form.component';
+import { UserResultComponent } from './user-result/user-result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchFormComponent
+    SearchFormComponent,
+    UserResultComponent
   ],
   imports: [
     BrowserModule,
