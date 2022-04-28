ea2803cc7acb5d82c63ad021aaaa63fceb263b68
janeffer945 <janeffernjeri945@gmail.com>
2022-04-28 09:29:19 +0300

    Auto-generated commit [ci skip]

--- src/app/repo.ts
+++
@@ -1,6 +0,0 @@
-export class Repo {
-    constructor(public clone_url:string,public name:string, public full_name:string,public html_url:string
-        ,public language:string ,public created_at:Date,public updated_at:Date,public description:string){
- 
-     }
- }
